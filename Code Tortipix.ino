59145d0a4e21c007aee346c79226d327e6db0bc2
Ascelefeu <120333499+Ascelefeu@users.noreply.github.com>
2024-05-05 23:04:31 +0200

    Update Tortipix, refonte condtion du poids

--- Code Tortipix.ino
+++ Code Tortipix.ino
@@ -1,4 +1,4 @@
-//Définition de la broche du servomoteur
+  //On inclue les bibliothèques servomoteur/écran lcd
   #include <Servo.h>
   #include <Wire.h>
   #include <LiquidCrystal_I2C.h>
@@ -11,35 +11,31 @@
 
   //Créer une instance de l'écran LCD
   LiquidCrystal_I2C lcd(LCD_ADDR, LCD_COLUMNS, LCD_ROWS);
-  
-  const int servoPin1 = 11; // Broche de contrôle du servomoteur
-  Servo servo1; // Création d'une instance du servomoteur
 
-  const int servoPin2 = 10; // Broche de contrôle du servomoteur
-  Servo servo2; // Création d'une instance du servomoteur
+  //On définit les broches des quatres servomoteurs
+  const int servoPin1 = 11;
+  Servo servo1; 
+  const int servoPin2 = 10;
+  Servo servo2; 
+  const int servoPin3 = 6; 
+  Servo servo3;
+  const int servoPin4 = 5; 
+  Servo servo4; 
 
-  const int servoPin3 = 6; // Broche de contrôle du servomoteur
-  Servo servo3; // Création d'une instance du servomoteur
-
-  const int servoPin4 = 5; // Broche de contrôle du servomoteur
-  Servo servo4; // Création d'une instance du servomoteur
+  //On instancie la borche du capteur de poids et le seuil
+  const int capteurPoidsPin = A5; 
+  const int seuilPoidsTropLourd=800;
 
   //Définition des broches du détecteur de distance
   #define Broche_Echo 12
   #define Broche_Trigger 13
 
   int angle = 90;
-
   int mesureMax = 300;
   int mesureMin = 3;
-
   long duree;
   long distance;
 
-      const int capteurPoidsPin = A5; // Broche analogique pour le capteur de poids
-      const int seuilPoidsTropLourd=800;
-  
-  
   //Défintion des flags pour l'electroaimant
   bool flageElect = false;
 
@@ -51,73 +47,64 @@
   
   void setup() {
     
-    //Définition des broches de sortie
-
-    //Pour le servomoteur
-
     // On attache chaque servo à sa broche
     servo1.attach(servoPin1); 
     servo2.attach(servoPin2);
     servo3.attach(servoPin3);
     servo4.attach(servoPin4);
-  
-    servo1.write(0); // Faire tourner le servomoteur de 90 degrés
+
+    //On initialise les servo à la position 0
+    servo1.write(0);
     servo2.write(0);
     servo3.write(0);
     servo4.write(0);
 
-    //affichage des instructions sur l'écran LCD
-   // Initialisation de l'écran LCD
-  lcd.init();
-  
-  // Augmenter la luminosité du rétroéclairage
-  lcd.setBacklight(255); // 255 représente la luminosité maximale
-  
-  // Affichage du message "Bonjour" sur l'écran LCD
-  lcd.setCursor(0, 0); // Position du curseur en haut à gauche
-    lcd.setCursor(0, 1); // Positionne le curseur à la deuxième ligne, première colonne
-    lcd.print("Mets tes affaires dans le coffre"); // Affiche votre deuxième phrase sur la deuxième ligne
+    // Initialisation de l'écran LCD
+    lcd.init();
+    lcd.setBacklight(255);
+    lcd.setCursor(0, 0); 
+    lcd.setCursor(0, 1); 
+
+    //Instruction à l'utilisateur
+    lcd.print("Mets tes affaires dans le coffre"); 
     delay(5000);
     lcd.clear();
 
     //Pin electroaimant
     pinMode(pinElec,OUTPUT);
 
+    //On définit les broches du capteur de distance
+    pinMode(Broche_Trigger, OUTPUT); 
+    pinMode(Broche_Echo, INPUT); 
+    Serial.begin (115200);
+
+    digitalWrite(pinElec,HIGH);
+
     //vérification du poids
-    
-    int poids = analogRead(capteurPoidsPin); // Lire la valeur du capteur de poids
+    int poids = analogRead(capteurPoidsPin); 
     while (poids > seuilPoidsTropLourd) {
     lcd.setCursor(0, 0);
     lcd.print("Poids trop lourd !");
-    digitalWrite(pinElec,HIGH);
+    poids = analogRead(capteurPoidsPin); 
+    delay(1500);
+    }
+
+    //Une fois le coffre rempli, on "ferme" l'electroaimant
+    delay(1000);
+    lcd.clear(); 
+    lcd.setCursor(0, 0);
+    lcd.print("Ma carapace est bien remplie !");
+    digitalWrite(pinElec,LOW);
     
    }
-   delay(1000);
-    lcd.clear(); // Efface l'écran LCD
-    lcd.setCursor(0, 0); // Positionne le curseur à la première ligne, première colonne
-    lcd.print("Mon coffre  bien rempli");
-
-   //On ferme l'electroaimant
-    digitalWrite(pinElec,LOW);
-
-
-    //On définit les broches du capteur de distance
-    pinMode(Broche_Trigger, OUTPUT); // Broche Trigger en sortie //
-    pinMode(Broche_Echo, INPUT); // Broche Echo en entree //
-    Serial.begin (115200);
-
-    
-
-    
-    
-
-  }
 
   void loop() {
 
+      //Tant qu'il n'y a pas d'obstacle, on avance
+      //Les pattes font un mouvements de vas et viens 2 à deux
       if(flagEnMarche)
       {
-        // Avancer
+        // Pattes avant avancent
       for (angle = 90; angle <= 120; angle++)
        {
         servo1.write(angle);
@@ -126,7 +113,7 @@
       }
       delay(500);
 
-      // Reculer
+      // Pattes avant reculent
       for (angle = 120; angle >= 60; angle--) {
         servo1.write(angle);
         servo4.write(angle);
@@ -134,7 +121,7 @@
       }
       delay(500);
 
-      // Tourner à gauche
+      // Pattes arrières avancent
       for (angle = 90; angle >= 60; angle--) {
         servo2.write(angle);
         servo3.write(angle);
@@ -142,7 +129,7 @@
       }
       delay(500);
 
-      // Tourner à droite
+      // Pattes arrières reculent
       for (angle = 60; angle <= 90; angle++) {
         servo2.write(angle);
         servo3.write(angle);
