83ae6e439c2453fd4a4732f35afba603e6af491e
Ascelefeu <120333499+Ascelefeu@users.noreply.github.com>
2024-05-29 15:53:55 +0200

    Proto final
    Modification de la méthode de locomotion, réagencement de la loop

--- Code Tortipix.ino
+++ Code Tortipix.ino
@@ -5,19 +5,20 @@
   //Définir l'adresse I2C de l'ecran
   #define LCD_ADDR 0x27
 
-  //Définir les dimensions de l'ecran LCD
+   //Définir les dimensions de l'ecran LCD
   #define LCD_COLUMNS 16
   #define LCD_ROWS 2
 
   //Créer une instance de l'écran LCD
   LiquidCrystal_I2C lcd(LCD_ADDR, LCD_COLUMNS, LCD_ROWS);
 
+
   //On définit les broches des quatres servomoteurs
-  const int servoPin1 = 11; //Arrière droit
+  const int servoPin1 = 11; //Avant droit
   Servo servo1; 
   const int servoPin2 = 6; //Arrière gauche
   Servo servo2; 
-  const int servoPin3 = 10; //Avant droit
+  const int servoPin3 = 10; //Arrière droit
   Servo servo3;
   const int servoPin4 = 5; //Avant gauche
   Servo servo4; 
@@ -30,7 +31,11 @@
   #define Broche_Echo 12
   #define Broche_Trigger 13
 
-  int angle = 90;
+  //Angle des servommoteurs
+  int angleAvant = 0;
+  int angleArriere = 0;
+
+  //Variables capteur de distance
   int mesureMax = 300;
   int mesureMin = 3;
   long duree;
@@ -53,20 +58,21 @@
     servo3.attach(servoPin3);
     servo4.attach(servoPin4);
 
-    //On initialise les servo à la position 0
-    servo1.write(0);
+    //On initialise les servo à la position 0 ou 45 car on a inversé des servo sur le robot
+    servo1.write(45);
     servo2.write(0);
-    servo3.write(0);
+    servo3.write(45);
     servo4.write(0);
 
-    // Initialisation de l'écran LCD
+     // Initialisation de l'écran LCD
     lcd.init();
     lcd.setBacklight(255);
     lcd.setCursor(0, 0); 
-    lcd.setCursor(0, 1); 
 
     //Instruction à l'utilisateur
-    lcd.print("Mets tes affaires dans le coffre"); 
+    lcd.print("Remplie"); 
+    lcd.setCursor(0, 1); 
+    lcd.print("le coffre");
     delay(5000);
     lcd.clear();
 
@@ -93,52 +99,55 @@
     delay(1000);
     lcd.clear(); 
     lcd.setCursor(0, 0);
-    lcd.print("Ma carapace est bien remplie !");
-    digitalWrite(pinElec,LOW);
+    lcd.print("Ma carapace ");
+    lcd.setCursor(0,1);
+    lcd.print("est remplie");
+    digitalWrite(pinElec,HIGH);
     
    }
 
+   
+
   void loop() {
 
-      //Tant qu'il n'y a pas d'obstacle, on avance
-      //Les pattes font un mouvements de vas et viens 2 à deux
-      if(flagEnMarche)
-      {
+    while(flagEnMarche)
+    {
+
         // Pattes avant avancent
-      for (angle = 90; angle <= 120; angle++)
-       {
-        servo1.write(angle);
-        servo4.write(angle);
-        delay(15);
-      }
-      delay(500);
+        for (angleAvant = 0; angleAvant <= 45; angleAvant++)
+        {
+          servo1.write(45-angleAvant);
+          servo4.write(angleAvant);
+          delay(15);
+        }
+        delay(500);
 
-      // Pattes avant reculent
-      for (angle = 120; angle >= 60; angle--) {
-        servo1.write(angle);
-        servo4.write(angle);
-        delay(15);
-      }
-      delay(500);
+        // Pattes arrières avancent
+        for (angleArriere = 0; angleArriere <= 45; angleArriere++) 
+        {
+          servo2.write(angleArriere);
+          servo3.write(45-angleArriere);
+          delay(15);
+        }
+        delay(500);
 
-      // Pattes arrières avancent
-      for (angle = 90; angle >= 60; angle--) {
-        servo2.write(angle);
-        servo3.write(angle);
-        delay(15);
-      }
-      delay(500);
+        // Toutes les pattes se replacent
+        for (angleAvant = 45; angleAvant >= 0; angleAvant--) 
+        {
+          servo1.write(45-angleAvant);
+          servo2.write(angleAvant);
+          servo3.write(45-angleAvant);
+          servo4.write(angleAvant);
+          delay(15);
+        }
 
-      // Pattes arrières reculent
-      for (angle = 60; angle <= 90; angle++) {
-        servo2.write(angle);
-        servo3.write(angle);
-        delay(15);
-      }
-    delay(500);
-    //mesure de la distance à l'aide du capteur
-      
-      // Debut de la mesure avec un signal de 10 μS applique sur TRIG //
+        angleAvant = 0;
+        angleArriere = 0;
+        delay(500);
+
+    
+
+    // Debut de la mesure avec un signal de 10 μS applique sur TRIG //
       digitalWrite(Broche_Trigger, LOW); // On efface l'etat logique de TRIG //
       delayMicroseconds(2);
       digitalWrite(Broche_Trigger, HIGH); // On met la broche TRIG a "1" pendant 10μS //
@@ -154,21 +163,11 @@
       // Verification si valeur mesuree dans la plage //
       if (distance >= mesureMax || distance <= mesureMin ||  analogRead(capteurPoidsPin) > seuilPoidsTropLourd) {
       flagEnMarche = false;
-      // Si la distance est hors plage, on affiche un message d'erreur //
-      //Serial.println("Distance de mesure en dehors de la plage (3 cm à 3 m)");
       }
-      }
-
-      //
-    
-    //else {
-    // Affichage dans le moniteur serie de la distance mesuree //
-    //Serial.print("Distance mesuree :");
-    //Serial.print(distance);
-    //Serial.println("cm");}
       
-    
-    //while(true)
-    //{}
+    }
+    digitalWrite(pinElec,HIGH);
+    while(true)
+    {}
 
   }
